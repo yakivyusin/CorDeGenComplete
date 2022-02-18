e270fff5677090f49e6bcae16c9d427aef36b43f
Yakiv Yusin <yusin.yakiv@gmail.com>
2022-02-18 14:23:23 +0200

    Add CLI Tool

---
+++ CorDeGen.CLI/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CorDeGen.CLI
+{
+    public class Program
+    {
+        public static void Main(int termCount, TermPresenterBuilder method, DirectoryInfo outputDirectory)
+        {
+            if (outputDirectory == null || !outputDirectory.Exists)
+            {
+                outputDirectory = new DirectoryInfo(".");
+            }
+
+            var generator = new CorpusGenerator(termCount, method.Presenter);
+            var corpus = generator.GetCorpus();
+
+            foreach (var generatedText in corpus.Select((text, index) => (text, index)))
+            {
+                File.WriteAllText(
+                    Path.Combine(outputDirectory.FullName, $"{generatedText.index}.txt"),
+                    generatedText.text);
+            }
+        }
+    }
+}
