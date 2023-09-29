0a41764b259deade91fc8ad2853886ea11745abe
Yakiv Yusin <yusin.yakiv@gmail.com>
2023-09-29 15:36:56 +0300

    Add Naive Parallel Generator

---
+++ CorDeGen/NaiveParallelCorpusGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace CorDeGen;
+
+public sealed class NaiveParallelCorpusGenerator
+{
+    private readonly int _termCount;
+    private readonly int _textCount;
+    private readonly int _r;
+    private readonly ITermPresenter _termPresenter;
+    private readonly int _parallelismDegree;
+
+    public NaiveParallelCorpusGenerator(int termCount, ITermPresenter termPresenter, int parallelismDegree)
+    {
+        _termCount = termCount;
+        _termPresenter = termPresenter;
+        _parallelismDegree = parallelismDegree;
+
+        _textCount = (int)Math.Pow(_termCount, 0.25);
+        _r = _textCount / 5 + 1;
+    }
+
+    public string[] GetCorpus()
+    {
+        var memory = _termCount * _textCount * _textCount / (_textCount / 5 + 2);
+        var texts = Enumerable.Range(0, _textCount).Select(x => new StringBuilder(memory)).ToArray();
+        var options = new ParallelOptions
+        {
+            MaxDegreeOfParallelism = _parallelismDegree
+        };
+
+        Parallel.For(0, _termCount, options, termIndex =>
+        {
+            var term = _termPresenter.GetTermPresentation(_termCount, termIndex);
+            var termTotalCount = _textCount * (termIndex % _textCount + 1);
+            var centralTextIndex = termIndex % _textCount;
+
+            for (int textIndex = centralTextIndex - _r; textIndex <= centralTextIndex + _r; textIndex++)
+            {
+                var termCount = termTotalCount / (2 * _r + 2);
+
+                if (textIndex == centralTextIndex)
+                {
+                    termCount *= 2;
+                }
+
+                var safeIndex = GetArraySafeIndex(textIndex);
+
+                lock (texts[safeIndex])
+                {
+                    texts[safeIndex].AppendLine(
+                        string.Join(
+                            ' ',
+                            Enumerable.Repeat(term, termCount)));
+                }
+            }
+        });
+
+        return texts.Select(x => x.ToString()).ToArray();
+    }
+
+    private int GetArraySafeIndex(int index) => index switch
+    {
+        int i when i < 0 => _textCount + i,
+        int i when i >= _textCount => i % _textCount,
+        _ => index
+    };
+}
