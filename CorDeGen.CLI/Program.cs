0287216976f4737e26d5630cd3756427b0fce9ba
Yakiv Yusin <yusin.yakiv@gmail.com>
2023-09-30 12:58:40 +0300

    Update CLI

--- CorDeGen.CLI/Program.cs
+++ CorDeGen.CLI/Program.cs
@@ -1,23 +1,29 @@
-﻿namespace CorDeGen.CLI
+﻿namespace CorDeGen.CLI;
+
+public class Program
 {
-    public class Program
+    public static void Main(int termCount, TermPresenterBuilder method, string generator, DirectoryInfo outputDirectory)
     {
-        public static void Main(int termCount, TermPresenterBuilder method, DirectoryInfo outputDirectory)
+        if (outputDirectory == null || !outputDirectory.Exists)
         {
-            if (outputDirectory == null || !outputDirectory.Exists)
-            {
-                outputDirectory = new DirectoryInfo(".");
-            }
+            outputDirectory = new DirectoryInfo(".");
+        }
 
-            var generator = new CorpusGenerator(termCount, method.Presenter);
-            var corpus = generator.GetCorpus();
+        var corpus = GenerateCorpus(generator, termCount, method.Presenter);
 
-            foreach (var generatedText in corpus.Select((text, index) => (text, index)))
-            {
-                File.WriteAllText(
-                    Path.Combine(outputDirectory.FullName, $"{generatedText.index}.txt"),
-                    generatedText.text);
-            }
+        foreach (var generatedText in corpus.Select((text, index) => (text, index)))
+        {
+            File.WriteAllText(
+                Path.Combine(outputDirectory.FullName, $"{generatedText.index}.txt"),
+                generatedText.text);
         }
     }
+
+    private static string[] GenerateCorpus(string generator, int termCount, ITermPresenter termPresenter) => generator switch
+    {
+        "default" => new CorpusGenerator(termCount, termPresenter).GetCorpus(),
+        "naive" => new NaiveParallelCorpusGenerator(termCount, termPresenter, Environment.ProcessorCount).GetCorpus(),
+        "parallel" => new ParallelCorpusGenerator(termCount, termPresenter, Environment.ProcessorCount).GetCorpus(),
+        _ => throw new ArgumentOutOfRangeException(nameof(generator))
+    };
 }
--- CorDeGen.CLI/TermPresenterBuilder.cs
+++ CorDeGen.CLI/TermPresenterBuilder.cs
@@ -1,20 +1,16 @@
-﻿namespace CorDeGen.CLI
+﻿namespace CorDeGen.CLI;
+
+public class TermPresenterBuilder
 {
-    public class TermPresenterBuilder
+    private readonly string _method;
+
+    public TermPresenterBuilder(string method) => _method = method;
+
+    public ITermPresenter Presenter => _method switch
     {
-        private readonly string _method;
-
-        public TermPresenterBuilder(string method)
-        {
-            _method = method;
-        }
-
-        public ITermPresenter Presenter => _method switch
-        {
-            "default" => ITermPresenter.Default,
-            "plus" => ITermPresenter.PlusEnglish,
-            "semantic" => ITermPresenter.Semantic,
-            _ => throw new ArgumentOutOfRangeException(nameof(_method))
-        };
-    }
+        "default" => ITermPresenter.Default,
+        "plus" => ITermPresenter.PlusEnglish,
+        "semantic" => ITermPresenter.Semantic,
+        _ => throw new ArgumentOutOfRangeException(nameof(_method))
+    };
 }
