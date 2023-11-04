dd06a59d65bf7755c8d1123ebcf7116dd115434f
Yakiv Yusin <yusin.yakiv@gmail.com>
2023-11-04 11:59:03 +0200

    Extend PB Tests to All Positive Ints

--- CorDeGen.Tests.Integration/NaiveParallelGeneratorTests.cs
+++ CorDeGen.Tests.Integration/NaiveParallelGeneratorTests.cs
@@ -5,63 +5,45 @@ public class NaiveParallelGeneratorTests
     [Property]
     public Property NaiveParallelGeneratorTest_DefaultPresenter(PositiveInt termCount)
     {
-        var property = () =>
-        {
-            var expectedTextBags = new CorpusGenerator(termCount.Get, ITermPresenter.Default).GetCorpus()
-                .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
+        var expectedTextBags = new CorpusGenerator(termCount.Get, ITermPresenter.Default).GetCorpus()
+            .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
 
-            var actualTextBags = new NaiveParallelCorpusGenerator(termCount.Get, ITermPresenter.Default, Environment.ProcessorCount).GetCorpus()
-                .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
+        var actualTextBags = new NaiveParallelCorpusGenerator(termCount.Get, ITermPresenter.Default, Environment.ProcessorCount).GetCorpus()
+            .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
 
-            return actualTextBags
-                .Zip(expectedTextBags)
-                .All(x => x.First.SequenceEqual(x.Second));
-        };
-
-        return property.When(
-            (256 <= termCount.Get && termCount.Get < 625) ||
-            termCount.Get >= 1296);
+        return actualTextBags
+            .Zip(expectedTextBags)
+            .All(x => x.First.SequenceEqual(x.Second))
+            .ToProperty();
     }
 
     [Property]
     public Property NaiveParallelGeneratorTest_PlusEnglishPresenter(PositiveInt termCount)
     {
-        var property = () =>
-        {
-            var expectedTextBags = new CorpusGenerator(termCount.Get, ITermPresenter.PlusEnglish).GetCorpus()
-                .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
+        var expectedTextBags = new CorpusGenerator(termCount.Get, ITermPresenter.PlusEnglish).GetCorpus()
+            .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
 
-            var actualTextBags = new NaiveParallelCorpusGenerator(termCount.Get, ITermPresenter.PlusEnglish, Environment.ProcessorCount).GetCorpus()
-                .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
+        var actualTextBags = new NaiveParallelCorpusGenerator(termCount.Get, ITermPresenter.PlusEnglish, Environment.ProcessorCount).GetCorpus()
+            .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
 
-            return actualTextBags
-                .Zip(expectedTextBags)
-                .All(x => x.First.SequenceEqual(x.Second));
-        };
-
-        return property.When(
-            (256 <= termCount.Get && termCount.Get < 625) ||
-            termCount.Get >= 1296);
+        return actualTextBags
+            .Zip(expectedTextBags)
+            .All(x => x.First.SequenceEqual(x.Second))
+            .ToProperty();
     }
 
     [Property]
     public Property NaiveParallelGeneratorTest_SemanticPresenter(PositiveInt termCount)
     {
-        var property = () =>
-        {
-            var expectedTextBags = new CorpusGenerator(termCount.Get, ITermPresenter.Semantic).GetCorpus()
-                .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
+        var expectedTextBags = new CorpusGenerator(termCount.Get, ITermPresenter.Semantic).GetCorpus()
+            .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
 
-            var actualTextBags = new NaiveParallelCorpusGenerator(termCount.Get, ITermPresenter.Semantic, Environment.ProcessorCount).GetCorpus()
-                .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
+        var actualTextBags = new NaiveParallelCorpusGenerator(termCount.Get, ITermPresenter.Semantic, Environment.ProcessorCount).GetCorpus()
+            .Select(x => x.Split(new[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).OrderBy(x => x));
 
-            return actualTextBags
-                .Zip(expectedTextBags)
-                .All(x => x.First.SequenceEqual(x.Second));
-        };
-
-        return property.When(
-            (256 <= termCount.Get && termCount.Get < 625) ||
-            termCount.Get >= 1296);
+        return actualTextBags
+            .Zip(expectedTextBags)
+            .All(x => x.First.SequenceEqual(x.Second))
+            .ToProperty();
     }
 }
--- CorDeGen.Tests.Integration/ParallelGeneratorTests.cs
+++ CorDeGen.Tests.Integration/ParallelGeneratorTests.cs
@@ -5,48 +5,27 @@ public class ParallelGeneratorTests
     [Property]
     public Property ParallelGeneratorTest_DefaultPresenter(PositiveInt termCount)
     {
-        var property = () =>
-        {
-            var expectedTexts = new CorpusGenerator(termCount.Get, ITermPresenter.Default).GetCorpus();
-            var actualTexts = new ParallelCorpusGenerator(termCount.Get, ITermPresenter.Default, Environment.ProcessorCount).GetCorpus();
+        var expectedTexts = new CorpusGenerator(termCount.Get, ITermPresenter.Default).GetCorpus();
+        var actualTexts = new ParallelCorpusGenerator(termCount.Get, ITermPresenter.Default, Environment.ProcessorCount).GetCorpus();
 
-            return actualTexts.SequenceEqual(expectedTexts);
-        };
-
-        return property.When(
-            (256 <= termCount.Get && termCount.Get < 625) ||
-            termCount.Get >= 1296);
+        return actualTexts.SequenceEqual(expectedTexts).ToProperty();
     }
 
     [Property]
     public Property ParallelGeneratorTest_PlusEnglishPresenter(PositiveInt termCount)
     {
-        var property = () =>
-        {
-            var expectedTexts = new CorpusGenerator(termCount.Get, ITermPresenter.PlusEnglish).GetCorpus();
-            var actualTexts = new ParallelCorpusGenerator(termCount.Get, ITermPresenter.PlusEnglish, Environment.ProcessorCount).GetCorpus();
+        var expectedTexts = new CorpusGenerator(termCount.Get, ITermPresenter.PlusEnglish).GetCorpus();
+        var actualTexts = new ParallelCorpusGenerator(termCount.Get, ITermPresenter.PlusEnglish, Environment.ProcessorCount).GetCorpus();
 
-            return actualTexts.SequenceEqual(expectedTexts);
-        };
-
-        return property.When(
-            (256 <= termCount.Get && termCount.Get < 625) ||
-            termCount.Get >= 1296);
+        return actualTexts.SequenceEqual(expectedTexts).ToProperty();
     }
 
     [Property]
     public Property ParallelGeneratorTest_SemanticPresenter(PositiveInt termCount)
     {
-        var property = () =>
-        {
-            var expectedTexts = new CorpusGenerator(termCount.Get, ITermPresenter.Semantic).GetCorpus();
-            var actualTexts = new ParallelCorpusGenerator(termCount.Get, ITermPresenter.Semantic, Environment.ProcessorCount).GetCorpus();
+        var expectedTexts = new CorpusGenerator(termCount.Get, ITermPresenter.Semantic).GetCorpus();
+        var actualTexts = new ParallelCorpusGenerator(termCount.Get, ITermPresenter.Semantic, Environment.ProcessorCount).GetCorpus();
 
-            return actualTexts.SequenceEqual(expectedTexts);
-        };
-
-        return property.When(
-            (256 <= termCount.Get && termCount.Get < 625) ||
-            termCount.Get >= 1296);
+        return actualTexts.SequenceEqual(expectedTexts).ToProperty();
     }
 }
