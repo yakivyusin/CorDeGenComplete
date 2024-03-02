50995fab91a861117f6f5627d819be649e6d9a26
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-03-02 11:40:07 +0200

    Add CorDeGen+(0-9) Presenter to CLI

--- CorDeGen.CLI/Cli.cs
+++ CorDeGen.CLI/Cli.cs
@@ -4,7 +4,7 @@ namespace CorDeGen.CLI;
 
 public class Cli
 {
-    private readonly Option<string> _termPresenterGlobalOption = new(name: "--method", description: "default | plus-en | plus-de | plus-fr | plus-it | semantic")
+    private readonly Option<string> _termPresenterGlobalOption = new(name: "--method", description: "default | plus-0-9 | plus-en | plus-de | plus-fr | plus-it | semantic")
     {
         IsRequired = true
     };
