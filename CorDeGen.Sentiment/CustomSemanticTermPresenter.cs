3312312a5a2b49a6fd95ab9b54e1c04c3b329512
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-07-20 12:44:52 +0300

    Add Sentiment Analysis Sample

---
+++ CorDeGen.Sentiment/CustomSemanticTermPresenter.cs
@@ -0,0 +1,11 @@
+﻿namespace CorDeGen.Sentiment;
+
+internal class CustomSemanticTermPresenter : ITermPresenter
+{
+    public string GetTermPresentation(int termsCount, int termIndex)
+    {
+        var textsCount = (int)Math.Pow(termsCount, 0.25);
+
+        return (termIndex % textsCount) < (textsCount / 2) ? "awful" : "amazing";
+    }
+}
---
+++ CorDeGen.Sentiment/Program.cs
@@ -0,0 +1,20 @@
+﻿using SimpleNetNlp;
+
+namespace CorDeGen.Sentiment;
+
+internal class Program
+{
+    static void Main(int termCount, bool useSemantic)
+    {
+        var generator = new CorpusGenerator(termCount, useSemantic ? new CustomSemanticTermPresenter() : ITermPresenter.Default);
+
+        var results = generator.GetCorpus(" ", ". ")
+            .Select(x => new Document(x))
+            .Select(d => d.Sentences.Select(s => s.Sentiment()).GroupBy(x => x));
+
+        foreach (var result in results)
+        {
+            Console.WriteLine(string.Join('|', result.OrderByDescending(x => x.Count()).Select(x => $"{x.Key}:{x.Count()}")));
+        }
+    }
+}
