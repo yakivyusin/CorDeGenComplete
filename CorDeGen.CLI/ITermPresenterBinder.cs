f5311b65e27473f8197ca757a17d21da14794452
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-11-02 11:37:15 +0200

    Refactor CLI

--- CorDeGen.CLI/ITermPresenterBinder.cs
+++ CorDeGen.CLI/ITermPresenterBinder.cs
@@ -1,36 +1,45 @@
-﻿using System.CommandLine;
+﻿using System.Collections.Frozen;
+using System.CommandLine;
 using System.CommandLine.Binding;
 
 namespace CorDeGen.CLI;
 
 public class ITermPresenterBinder : BinderBase<ITermPresenter>
 {
+    private static readonly FrozenDictionary<string, Func<ITermPresenter>> _factories = new Dictionary<string, Func<ITermPresenter>>()
+    {
+        ["default"] = () => ITermPresenter.Default,
+        ["plus"] = () => ITermPresenter.Plus.WesternEurope.English,
+        ["plus-0-9"] = () => ITermPresenter.Plus.Digits,
+        ["plus-en"] = () => ITermPresenter.Plus.WesternEurope.English,
+        ["plus-de"] = () => ITermPresenter.Plus.CentralEurope.German,
+        ["plus-fr"] = () => ITermPresenter.Plus.WesternEurope.French,
+        ["plus-it"] = () => ITermPresenter.Plus.SouthernEurope.Italian,
+        ["plus-da"] = () => ITermPresenter.Plus.NorthernEurope.Danish,
+        ["plus-is"] = () => ITermPresenter.Plus.NorthernEurope.Icelandic,
+        ["plus-lt"] = () => ITermPresenter.Plus.NorthernEurope.Lithuanian,
+        ["plus-smj"] = () => ITermPresenter.Plus.NorthernEurope.LuleSami,
+        ["plus-se"] = () => ITermPresenter.Plus.NorthernEurope.NorthernSami,
+        ["plus-no"] = () => ITermPresenter.Plus.NorthernEurope.Norwegian,
+        ["plus-sv"] = () => ITermPresenter.Plus.NorthernEurope.Swedish,
+        ["plus-cs"] = () => ITermPresenter.Plus.CentralEurope.Czech,
+        ["plus-hu"] = () => ITermPresenter.Plus.CentralEurope.Hungarian,
+        ["plus-pl"] = () => ITermPresenter.Plus.CentralEurope.Polish,
+        ["plus-sk"] = () => ITermPresenter.Plus.CentralEurope.Slovak,
+        ["plus-sl"] = () => ITermPresenter.Plus.CentralEurope.Slovenian,
+        ["semantic"] = () => ITermPresenter.Semantic,
+    }.ToFrozenDictionary();
+
+    public static IEnumerable<string> SupportedKeys => _factories.Keys;
+
     private readonly Option<string> _termPresenterOption;
 
     public ITermPresenterBinder(Option<string> termPresenterOption) => _termPresenterOption = termPresenterOption;
 
-    protected override ITermPresenter GetBoundValue(BindingContext bindingContext) => bindingContext.ParseResult.GetValueForOption(_termPresenterOption) switch
-    {
-        "default" => ITermPresenter.Default,
-        "plus" => ITermPresenter.Plus.WesternEurope.English,
-        "plus-0-9" => ITermPresenter.Plus.Digits,
-        "plus-en" => ITermPresenter.Plus.WesternEurope.English,
-        "plus-de" => ITermPresenter.Plus.CentralEurope.German,
-        "plus-fr" => ITermPresenter.Plus.WesternEurope.French,
-        "plus-it" => ITermPresenter.Plus.SouthernEurope.Italian,
-        "plus-da" => ITermPresenter.Plus.NorthernEurope.Danish,
-        "plus-is" => ITermPresenter.Plus.NorthernEurope.Icelandic,
-        "plus-lt" => ITermPresenter.Plus.NorthernEurope.Lithuanian,
-        "plus-smj" => ITermPresenter.Plus.NorthernEurope.LuleSami,
-        "plus-se" => ITermPresenter.Plus.NorthernEurope.NorthernSami,
-        "plus-no" => ITermPresenter.Plus.NorthernEurope.Norwegian,
-        "plus-sv" => ITermPresenter.Plus.NorthernEurope.Swedish,
-        "plus-cs" => ITermPresenter.Plus.CentralEurope.Czech,
-        "plus-hu" => ITermPresenter.Plus.CentralEurope.Hungarian,
-        "plus-pl" => ITermPresenter.Plus.CentralEurope.Polish,
-        "plus-sk" => ITermPresenter.Plus.CentralEurope.Slovak,
-        "plus-sl" => ITermPresenter.Plus.CentralEurope.Slovenian,
-        "semantic" => ITermPresenter.Semantic,
-        _ => ITermPresenter.Default
-    };
+    protected override ITermPresenter GetBoundValue(BindingContext bindingContext) =>
+        _factories.TryGetValue(bindingContext.ParseResult.GetValueForOption(_termPresenterOption) ?? string.Empty, out var factory) switch
+        {
+            true => factory(),
+            false => ITermPresenter.Default
+        };
 }
