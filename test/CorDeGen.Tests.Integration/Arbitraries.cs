338092a83460b14da06ac0182b10fd2426859a92
Yakiv Yusin <yusin.yakiv@gmail.com>
2025-03-17 16:47:57 +0200

    Refactor to Arbitrary Type

---
+++ test/CorDeGen.Tests.Integration/Arbitraries.cs
@@ -0,0 +1,10 @@
+﻿namespace CorDeGen.Tests.Integration
+{
+    public class Arbitraries
+    {
+        public static Arbitrary<int> CorDeGenDomain()
+        {
+            return Arb.From(Gen.Choose(1296, 25000));
+        }
+    }
+}
--- test/CorDeGen.Tests.Integration/DefaultPresenterExactEstimatorTests.cs
+++ test/CorDeGen.Tests.Integration/DefaultPresenterExactEstimatorTests.cs
@@ -2,54 +2,51 @@
 
 public class DefaultPresenterExactEstimatorTests
 {
-    [Property(StartSize = 1296, EndSize = 25000, MaxTest = 200)]
-    public Property Sequential_GeneratedTextsLength_MatchEstimation(PositiveInt termCount)
+    [Property(Arbitrary = [typeof(Arbitraries)], MaxTest = 200)]
+    public Property Sequential_GeneratedTextsLength_MatchEstimation(int termCount)
     {
         var estimator = ITermPresenter.Default.GetTextLengthEstimator();
-        var corpus = new CorpusGenerator(termCount.Get, ITermPresenter.Default).GetCorpus(" ", " ");
+        var corpus = new CorpusGenerator(termCount, ITermPresenter.Default).GetCorpus(" ", " ");
         var r = corpus.Length / 5 + 1;
         var estimations = Enumerable.Range(0, corpus.Length)
-            .Select(x => estimator.Estimate(termCount.Get, corpus.Length, r, 1, 1, x))
+            .Select(x => estimator.Estimate(termCount, corpus.Length, r, 1, 1, x))
             .ToArray();
 
         return corpus
             .Select(x => x.Length)
             .SequenceEqual(estimations)
-            .ToProperty()
-            .When(termCount.Get >= 1296);
+            .ToProperty();
     }
 
-    [Property(StartSize = 1296, EndSize = 25000, MaxTest = 200)]
-    public Property NaiveParallel_GeneratedTextsLength_MatchEstimation(PositiveInt termCount)
+    [Property(Arbitrary = [typeof(Arbitraries)], MaxTest = 200)]
+    public Property NaiveParallel_GeneratedTextsLength_MatchEstimation(int termCount)
     {
         var estimator = ITermPresenter.Default.GetTextLengthEstimator();
-        var corpus = new NaiveParallelCorpusGenerator(termCount.Get, ITermPresenter.Default, Environment.ProcessorCount).GetCorpus();
+        var corpus = new NaiveParallelCorpusGenerator(termCount, ITermPresenter.Default, Environment.ProcessorCount).GetCorpus();
         var r = corpus.Length / 5 + 1;
         var estimations = Enumerable.Range(0, corpus.Length)
-            .Select(x => estimator.Estimate(termCount.Get, corpus.Length, r, 1, Environment.NewLine.Length, x))
+            .Select(x => estimator.Estimate(termCount, corpus.Length, r, 1, Environment.NewLine.Length, x))
             .ToArray();
 
         return corpus
             .Select(x => x.Length)
             .SequenceEqual(estimations)
-            .ToProperty()
-            .When(termCount.Get >= 1296);
+            .ToProperty();
     }
 
-    [Property(StartSize = 1296, EndSize = 25000, MaxTest = 200)]
-    public Property Parallel_GeneratedTextsLength_MatchEstimation(PositiveInt termCount)
+    [Property(Arbitrary = [typeof(Arbitraries)], MaxTest = 200)]
+    public Property Parallel_GeneratedTextsLength_MatchEstimation(int termCount)
     {
         var estimator = ITermPresenter.Default.GetTextLengthEstimator();
-        var corpus = new ParallelCorpusGenerator(termCount.Get, ITermPresenter.Default, Environment.ProcessorCount).GetCorpus();
+        var corpus = new ParallelCorpusGenerator(termCount, ITermPresenter.Default, Environment.ProcessorCount).GetCorpus();
         var r = corpus.Length / 5 + 1;
         var estimations = Enumerable.Range(0, corpus.Length)
-            .Select(x => estimator.Estimate(termCount.Get, corpus.Length, r, 1, Environment.NewLine.Length, x))
+            .Select(x => estimator.Estimate(termCount, corpus.Length, r, 1, Environment.NewLine.Length, x))
             .ToArray();
 
         return corpus
             .Select(x => x.Length)
             .SequenceEqual(estimations)
-            .ToProperty()
-            .When(termCount.Get >= 1296);
+            .ToProperty();
     }
 }
